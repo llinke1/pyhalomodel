d02967eef6bcec427dd82dc516ac0a48f4688544
Laila <lailalinke95@gmail.com>
2023-10-04 15:53:46 +0200

    Removed camb dependence and added fit code

--- pyhalomodel/hmcode.py
+++ pyhalomodel/hmcode.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import pyhalomodel as halo
 from pyhalomodel import model, window_function, profile, concentration
+import pyccl as ccl
 
-def BiHMCode_bispectrum(ks, Pks_lin, Ms, camb_results, Omega_m=0.3, z=0, ingredients={'hmf': 'Sheth & Tormen (1999)', 'concentration':'Duffy et al. (2008)',
+def BiHMCode_bispectrum(ks, Ms, cosmology, z=0, ingredients={'hmf': 'Sheth & Tormen (1999)', 'concentration':'Duffy et al. (2008)',
                                                                'halo definition':'Mvir', 'profile':'NFW'},
                         freePars={'eta':0, 'B':4, 'alpha1':1, 'alpha2':1, 'kstar': 0},
-                        verbose=False, fastCalc=False, onlyEquilateral=True):
-    hmod = model(z, Omega_m, name=ingredients['hmf'])
+                        verbose=False, fastCalc=False, onlyEquilateral=True, dewiggle=True):
+    hmod = model(z, cosmology["Omega_m"], name=ingredients['hmf'])
     if verbose:
         print(hmod)
 
     Rs = hmod.Lagrangian_radius(Ms)
-    sigmaRs = camb_results.get_sigmaR(Rs, hubble_units=True, return_R_z=False)[[z].index(z)]
+    #sigmaRs = camb_results.get_sigmaR(Rs, hubble_units=True, return_R_z=False)[[z].index(z)]
+    a=1/(1+z)
+    sigmaRs=ccl.sigmaR(cosmology, Rs/cosmology['h'], a)
     rvs = hmod.virial_radius(Ms)
 
     nus=hmod._peak_height(Ms, sigmaRs)
@@ -25,7 +28,30 @@ def BiHMCode_bispectrum(ks, Pks_lin, Ms, camb_results, Omega_m=0.3, z=0, ingredi
     if verbose:
         print(matter_profile)
 
-    Bi_3h, Bi_2h, Bi_1h, Sum=hmod.bispectrum(ks, Pks_lin, Ms, sigmaRs, {'m': matter_profile}, verbose=verbose, fastCalc=fastCalc, 
+    Pk_lin=ccl.linear_matter_power(cosmology, ks*cosmology['h'], a)
+    
+    if dewiggle:
+        from scipy.ndimage import gaussian_filter1d
+        dlnk = np.log(ks[1]/ks[0])    
+        sigma = 0.25/dlnk
+
+        cosmoNoWiggle=ccl.Cosmology(Omega_c=cosmology["Omega_c"], Omega_b=cosmology["Omega_b"], h=cosmology["h"], n_s=cosmology["n_s"], sigma8=cosmology["sigma8"],
+                                transfer_function="eisenstein_hu_nowiggles")
+        Pk_NoWiggle=ccl.linear_matter_power(cosmoNoWiggle, ks*cosmology['h'], a)
+
+        Pk_ratio=Pk_lin/Pk_NoWiggle
+        Pk_ratio=gaussian_filter1d(Pk_ratio, sigma)
+        Pk_smooth=Pk_ratio*Pk_NoWiggle
+        Pk_Wiggle=Pk_lin-Pk_smooth
+
+        sigmaV=np.trapz(Pk_lin, ks)/6/np.pi**2
+        f=np.exp(-ks*ks*sigmaV)
+
+        Pk_lin=Pk_lin-(1-f)*Pk_Wiggle
+
+    Pk_lin*=cosmology['h']**3
+
+    Bi_3h, Bi_2h, Bi_1h, Sum=hmod.bispectrum(ks, Pk_lin, Ms, sigmaRs, {'m': matter_profile}, verbose=verbose, fastCalc=fastCalc, 
                                              onlyEquilateral=onlyEquilateral, kstar=freePars['kstar'], 
                                              f=freePars['f'], kd=freePars['kd'], nd=freePars['nd'])
     #print(freePars['alpha1'])
